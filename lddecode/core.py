3e55aa2740cfde735cb13593d6b6916fe0b9fec2
Chad <Chad.Page@gmail.com>
2026-05-25 07:11:08 -0700

    turns out nb_concatenate was a Bad Idea.
    (cherry picked from commit 980f3a2d38ebdcd04256514173172e71b8465077)

--- lddecode/core.py
+++ lddecode/core.py
@@ -33,7 +33,7 @@ from .utils import findpeaks, findpulses, calczc, inrange, roundfloat, refine_pi
 from .utils import LRUupdate, clb_findbursts, angular_mean_helper, phase_distance
 from .utils import build_hilbert, unwrap_hilbert, unwrap_hilbert_pll, emphasis_iir, filtfft
 from .utils import fft_do_slice, fft_determine_slices, StridedCollector, hz_to_output_array
-from .utils import Pulse, nb_std, nb_gt, n_ornotrange, nb_concatenate, gen_bpf_supergauss, FieldInfo
+from .utils import Pulse, nb_std, nb_gt, n_ornotrange, gen_bpf_supergauss, FieldInfo
 
 try:
     # If Anaconda's numpy is installed, mkl will use all threads for fft etc
@@ -1815,7 +1815,7 @@ class DemodCache:
 
         rv = {}
         for k in t.keys():
-            rv[k] = nb_concatenate(t[k]) if len(t[k]) else None
+            rv[k] = np.concatenate(t[k]) if len(t[k]) else None
 
         if rv["audio"] is not None:
             rv["audio_phase1"] = rv["audio"]
